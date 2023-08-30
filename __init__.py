34a9168d6c02104ceb90bea27bb15bf7466cae56
Bruno Madeira <bmad.works@gmail.com>
2023-08-30 15:04:21 +0100

    fix loading behavior.

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ try:
     from .api_nodes import NODE_CLASS_MAPPINGS as api
     loaded.append(f"api nodes ({len([*api])})")
 except Exception as e:
-    repeat = {}
+    api = {}
     not_loaded.append("api nodes")
     exceptions.append(e)
 
@@ -17,7 +17,7 @@ try:
     from .simple_utilities import NODE_CLASS_MAPPINGS as simple
     loaded.append(f"simple utility nodes ({len([*simple])})")
 except Exception as e:
-    cc_ade20k = {}
+    simple = {}
     exceptions.append(e)
     not_loaded.append("simple utility nodes")
 
@@ -25,7 +25,7 @@ try:
     from .cv_nodes import NODE_CLASS_MAPPINGS as cv_nodes
     loaded.append(f"CV nodes ({len([*cv_nodes])})")
 except Exception as e:
-    grab = {}
+    cv_nodes = {}
     not_loaded.append("CV nodes")
     exceptions.append(e)
 
@@ -56,18 +56,20 @@ except Exception as e:
 
 import __main__
 import os
-import nodes
 
 NODE_CLASS_MAPPINGS = {**api, **simple, **cv_nodes, **cc_ade20k, **otsu, **extended}
-WEB_DIRECTORY = "js"
 
 __all__ = ['NODE_CLASS_MAPPINGS']
+
+extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)), "web\extensions\Bmad")
+
 
 
 print('\n\033[1mFrom Bmad Custom Nodes\033[0m')
 
-if not hasattr(nodes, "EXTENSION_WEB_DIRS"):
-   exceptions.append(f"The version of ComfyUI is outdated. Some nodes won't work as intended.")
+if not os.path.exists(extentions_folder):
+    exceptions.append('"web\extensions\Bmad" folder is missing, some nodes won\'t work as intended.')
+
 
 print(f' \033[92mLoaded {len([*NODE_CLASS_MAPPINGS])} nodes:')
 for m in loaded:
