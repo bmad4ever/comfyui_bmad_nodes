7c10d1873939a4157754108c8f7b8f7cac6e9914
Bruno Madeira <bmad.works@gmail.com>
2023-12-28 17:43:13 +0000

    updated mask_outer_blur.py:
    - cupyx solution now avoids corners so that the result is equal to cpu implementation;
    - cupyx solution now receives kernel instead of using comparing distance to the pixel;
    - cpu implementation pre-computes valid kernel indices prior to computing chuncks.

--- utils/mask_outer_blur.py
+++ utils/mask_outer_blur.py
@@ -3,45 +3,35 @@ from cupyx import jit
 
 
 @jit.rawkernel()
-def blur_cuda(src, mask, out, kernel_s, w, h):
+def blur_cuda(src, mask, out, kernel, kernel_s, w, h):
     tidx = jit.blockIdx.x * jit.blockDim.x + jit.threadIdx.x
     tidy = jit.blockIdx.y * jit.blockDim.y + jit.threadIdx.y
 
-    kernel_r2 = (kernel_s / 2) ** 2
+    kernel_r = kernel_s // 2
     ntidx = jit.gridDim.x * jit.blockDim.x
     ntidy = jit.gridDim.y * jit.blockDim.y
-
-    for j in range(tidy, h, ntidy):
-        ymin = max(j - kernel_s // 2, 0)
-        ymax = min(j + kernel_s // 2, h - 1)
-        for i in range(tidx, w, ntidx):
+    for j in range(tidy + kernel_r, h - kernel_r, ntidy):
+        for i in range(tidx + kernel_r, w - kernel_r, ntidx):
             if mask[j, i] == 0.0:
-                xmin = max(i - kernel_s // 2, 0)
-                xmax = min(i + kernel_s // 2, w - 1)
                 count = 0.0
                 avg_c0 = 0.0
                 avg_c1 = 0.0
                 avg_c2 = 0.0
-                for y in range(ymin, ymax):
-                    for x in range(xmin, xmax):
-                        if mask[y, x] > 0.0:
-                            dist2 = (j - y) ** 2 + (i - x) ** 2
-                            if dist2 <= kernel_r2:
-                                count += 1.0
-                                avg_c0 = avg_c0 * (count - 1) / count + src[y, x, 0] / count
-                                avg_c1 = avg_c1 * (count - 1) / count + src[y, x, 1] / count
-                                avg_c2 = avg_c2 * (count - 1) / count + src[y, x, 2] / count
-                out[j, i, 0] = avg_c0
-                out[j, i, 1] = avg_c1
-                out[j, i, 2] = avg_c2
+                for y in range(-kernel_r, kernel_r + 1):
+                    for x in range(-kernel_r, kernel_r + 1):
+                        if kernel[y + kernel_r, x + kernel_r] > 0.0 and mask[j + y, i + x] > 0.0:
+                            count += 1.0
+                            avg_c0 = avg_c0 * (count-1) / count + src[j + y, i + x, 0] / count
+                            avg_c1 = avg_c1 * (count-1) / count + src[j + y, i + x, 1] / count
+                            avg_c2 = avg_c2 * (count-1) / count + src[j + y, i + x, 2] / count
+                oj = j - kernel_r
+                oi = i - kernel_r
+                out[oj, oi, 0] = avg_c0
+                out[oj, oi, 1] = avg_c1
+                out[oj, oi, 2] = avg_c2
 
 
-def blur_cpu(src, mask, kernel_s, w, h):
-    """
-    IMPORTANT DEV-NOTE: unlike GPU solution, src image corners are skipped;
-    this way relevant kernel indexes are precomputed
-    and there are no distance computations within the inner loop
-    """
+def blur_cpu(src, mask, kernel, kernel_s, w, h):
     import multiprocessing
     from joblib import Parallel, delayed
     # this is safe, right? ( --___-- );
@@ -49,12 +39,6 @@ def blur_cpu(src, mask, kernel_s, w, h):
     # prob. a good idea to avoid using all cores.
 
     # region auxiliary methods
-
-    def precompute_kernel_indices(kernel_r):
-        y, x = np.ogrid[-kernel_r:kernel_r + 1, -kernel_r:kernel_r + 1]
-        mask = x ** 2 + y ** 2 <= kernel_r ** 2
-        indices = np.array(np.where(mask)).T
-        return indices - [kernel_r, kernel_r]
 
     def calculate_average_color(src, mask, kernel_indices, j, i):
         valid_indices = kernel_indices + [j, i]
@@ -66,9 +50,8 @@ def blur_cpu(src, mask, kernel_s, w, h):
         return avg_c
 
     def process_chunk(args):
-        src, mask, kernel_s, w, h, start_row, end_row = args
+        src, mask, kernel_indices, kernel_s, w, h, start_row, end_row = args
         kernel_r = kernel_s // 2
-        kernel_indices = precompute_kernel_indices(kernel_r)
         out_chunk = np.zeros((end_row - start_row, w, src.shape[2]))
 
         for j in range(max(start_row, kernel_r), min(end_row, h - kernel_r)):
@@ -82,13 +65,15 @@ def blur_cpu(src, mask, kernel_s, w, h):
     # endregion
 
     # setup args
+    kernel_indices = np.transpose(np.nonzero(kernel)) - kernel_s // 2
     num_cores = multiprocessing.cpu_count()
     num_cores = max(1, num_cores-1)  # leave 1 core unoccupied in case computations take forever
     chunk_size = h // num_cores
-    chunks = [(src, mask, kernel_s, w, h, i * chunk_size, (i + 1) * chunk_size) for i in range(num_cores)]
+
+    chunks = [(src, mask, kernel_indices, kernel_s, w, h, i * chunk_size, (i + 1) * chunk_size) for i in range(num_cores)]
     # include any leftover rows
     if h % num_cores != 0:
-        chunks[-1] = (src, mask, kernel_s, w, h, (num_cores - 1) * chunk_size, h)
+        chunks[-1] = (src, mask, kernel_indices, kernel_s, w, h, (num_cores - 1) * chunk_size, h)
 
     # compute using available cores
     with Parallel(n_jobs=num_cores) as parallel:
